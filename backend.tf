d1500d179d5f140e7c06de5b6815ce4a2ff91569
blaise <kaziagba@gmail.com>
2024-08-01 22:45:26 -0400

    terraform week10

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "w7-la-terraform"   #replace with your bucket
+    bucket = "wwwww77777"   #replace with your bucket
     key    = "week10/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
-    dynamodb_table = "locktable" #replace with the table
+    dynamodb_table = "loooooooo" #replace with the table
   }
 }
